a546492635634ad7f1b57e3a3b09dd6ba5f5f5a1
Sahidul Islam <jonyahmed19@gmail.com>
2023-01-24 22:23:22 +0600

    fixes

--- src/components/Todo-crud/TodoCrud.js
+++ src/components/Todo-crud/TodoCrud.js
@@ -4,7 +4,7 @@ import CreateTodo from './CreateTodo';
 const TodoCrud = () => {
     return (
         <div>
-            <h1>Todo CRUD</h1>
+            <h1>Redux Todo CRUD</h1>
 
             <CreateTodo></CreateTodo>
         </div>
