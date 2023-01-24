ee414f805c84e7012632394d47d9cdd5a61dfb73
Sahidul Islam <jonyahmed19@gmail.com>
2023-01-24 22:21:39 +0600

    fixes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Stack } from "@mui/material";
+import TodoCrud from "./components/Todo-crud/TodoCrud";
+
+
+function App() {
+  return (
+    <Stack>
+     <TodoCrud></TodoCrud>
+    </Stack>
+  );
+}
+
+export default App;
